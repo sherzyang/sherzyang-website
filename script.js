7de03ea05fa7715ac2582e4caf9675dd3ee595bc
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-08 11:58:34 -0800

    Update with profile photo.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+p.textContent = "I am based in Washington, USA. I grew up outside of Philadelphia and have a 4 year old min pin terrier mix named Teddy Roosevelt."
+document.querySelector("body").appendChild(p);
